e1fd225a1edf94a6a55606f1a82d38599765ff12
sesayhassan <sesayhassan583@gmail.com>
2022-12-21 18:48:27 +0000

    forth commit

---
+++ app.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const projectOneBtn = document.querySelector(".p1btn");
+const projectTwoBtn = document.querySelector(".p2btn");
+const projectThreeBtn = document.querySelector(".p3btn");
+
+projectOneBtn.addEventListener("click",() => {
+ alert(
+    "this meal app is under developement, please check in some other time");
+});
+
+
+projectTwoBtn.addEventListener("click", () => {
+  alert(
+    "this meal app is under developement, please check in some other time");
+});
+
+
+projectThreeBtn.addEventListener("click", () => {
+  alert(
+    "this meal app is under developement, please check in some other time");
+});
+
